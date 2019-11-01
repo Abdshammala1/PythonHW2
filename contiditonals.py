ed19d21b79a542a241d763e09accfe7aa61e9a6a
Abdshammala1 <55294151+Abdshammala1@users.noreply.github.com>
2019-11-01 13:03:03 -0400

    Add files via upload

---
+++ contiditonals.py
@@ -0,0 +1,10 @@
+temperature = input ("input a number between0 and 100")
+
+if temperature <=4:
+	print("water is a solid. cu its frozen")
+
+if temperature <100:
+	print("water is liquid")
+
+if temperature >=100:
+	print("water is gas. cuz its boiling")
